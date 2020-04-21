2c381a096d5e156d2d33d4d508e93158481c09b7
mappleKeuj <34816176+mappleKeuj@users.noreply.github.com>
2020-04-21 07:32:59 -0400

    Revert "c"
    This reverts commit 6fd5bc89f3a30babd4410a375037b9c20bce3b7a.

--- construction.cc
+++ construction.cc
@@ -23,12 +23,6 @@ public:
 		 couleur = c;
 	 }
 	 
-	 Brique()
-	 {
-		forme = "                 ";
-		couleur = "";
-	 }
-	 
 	 ostream& afficher(ostream& sortie) const;
 };
 ostream& Brique::afficher(ostream& sortie) const{
@@ -59,48 +53,53 @@ class Construction
 		
 	ostream& afficher(ostream& sortie) const;
 	
-	const Construction operator^=(Construction const& construction);
-	const Construction operator-=(Construction const& construction);
-	const Construction operator+=(Construction const& construction);
+	const Construction operator^=(Construction construction);
 };
 
-const Construction Construction::operator^=(Construction const& c) {
-	for (size_t i = 0; i < c.contenu.size(); i++){
-		contenu.push_back(c.contenu.at(i));
+const Construction Construction::operator^=(Construction construction) {
+	cout << "Construction contenu : " << endl;
+	cout << "size i : " << construction.contenu.size() << endl;
+	cout << "size j : " << construction.contenu[0].size() << endl;
+	cout << "size k : " << construction.contenu[0][0].size() << endl;
+		
+	construction.contenu[0].push_back(construction.contenu[0].at(0));
+	//~ construction.contenu[0].push_back(construction.contenu[0].at(0));
+	
+	construction.contenu[0][0].push_back(construction.contenu[0][0].at(0));
+	construction.contenu[0][0].push_back(construction.contenu[0][0].at(0)); 
+	
+	cout << construction.contenu[0][1].at(2) << endl; 
+	
+	cout << "size i : " << construction.contenu.size() << endl;
+	cout << "size j : " << construction.contenu[0].size() << endl;
+	cout << "size k : " << construction.contenu[0][0].size() << endl;
+
+	cout << "Contenu : " << endl;
+
+	cout << "size i : " << contenu.size() << endl;
+	cout << "size j : " << contenu[0].size() << endl;
+	cout << "size k : " << contenu[0][0].size() << endl;
+	
+	// Padding with empty brique to avoid memory error
+	if (construction.contenu[0][0].size() > contenu[0][0].size()){
+		int padding = construction.contenu[0][0].size() - contenu[0][0].size();
+		for (int p = 0; p < padding; p++){
+			contenu[0][0].push_back(Brique ("                 ", ""));
+		}
 	}
 	
-	//~ contenu.resize(c.contenu.size() + contenu.size(), 
-	//~ vector<vector<Brique>>(c.contenu[0].size() + contenu[0].size()));
+	// add constructions
+	for (unsigned int i = 0; i < construction.contenu.size(); i++){
+		contenu.push_back(construction.contenu.at(i));
+	}	
+	for (unsigned int j = 0; j < construction.contenu[0].size(); j++){
+		contenu[0].push_back(construction.contenu[0].at(j));
+	}	
+	cout << "size i : " << contenu.size() << endl;
+	cout << "size j : " << contenu[0].size() << endl;
+	cout << "size k : " << contenu[0][0].size() << endl;
 
 	return *this;
-}
-
-const Construction Construction::operator-=(Construction const& construction) {
-	if (construction.contenu.size() >= contenu.size()){
-		for (size_t i = 0; i < construction.contenu.size(); i++){
-			for (size_t j = 0; j < construction.contenu[i].size(); j++){
-				contenu[i].push_back(construction.contenu[i].at(j));
-			}
-		}
-	}
-
-	return *this;
-}
-
-const Construction Construction::operator+=(Construction const& c){
-	if (c.contenu.size() >= contenu.size()){
-		for (size_t i = 0; i < c.contenu.size(); i++){
-			for (size_t j = 0; j < c.contenu[i].size(); j++){
-				for (size_t k = 0; k < c.contenu[i][j].size(); k++){
-					if (c.contenu[i][j].size() >= contenu[i][j].size()){
-						contenu[i][j].push_back(c.contenu[i][j].at(k));
-					}
-				}
-			}
-		}
-	}
-
-	return *this;	
 }
 
 ostream& Construction::afficher(ostream& sortie) const{
@@ -119,50 +118,17 @@ ostream& operator<<(ostream& sortie, Construction const& contenu) {
 	return contenu.afficher(cout);
 }
 
-const Construction operator^(Construction a, Construction const& b){
-	a ^= b;
-	return a;
-}
+//~ const Construction operator*(unsigned int n, Construction const& a)
+//~ {
+//~ }
 
-Construction operator-(Construction a, Construction const & b){
-	a -= b;
-	return a;
-}
+//~ const Construction operator/(unsigned int n, Construction const& a)
+//~ {
+//~ }
 
-Construction operator+(Construction a, Construction const & b){
-	a += b;
-	return a;
-}
-
-const Construction operator*(unsigned int n, Construction const& a)
-{
-	Construction b(a.contenu[0][0][0]);
-	for (unsigned int i = 0; i < n; i++) {
-		b+=a;
-	}
-	
-	return b;
-}
-
-const Construction operator/(unsigned int n, Construction const& a)
-{
-	Construction b(a.contenu[0][0][0]);
-	for (unsigned int i = 0; i < n; i++) {
-		b^=a;
-	}
-	
-	return b;
-}
-
-const Construction operator%(unsigned int n, Construction const& a)
-{
-	Construction b(a.contenu[0][0][0]);
-	for (unsigned int i = 0; i < n; i++) {
-		b-=a;
-	}
-	
-	return b;
-}
+//~ const Construction operator%(unsigned int n, Construction const& a)
+//~ {
+//~ }
 
 /*******************************************
  * Ne rien modifier après cette ligne.
@@ -176,17 +142,11 @@ int main()
   Brique toitM(" pleine ", "rouge");
   Brique mur(" pleine ", "blanc");
   Brique vide ("                 ", "");
-	Construction mur_construction(4*mur);
-	//~ Construction toit(toitG);
-	//~ cout << "Operator ^= : " << endl;
-	//~ mur_construction^=toit;
+	Construction mur_construction(mur);
+	Construction toit(toitM);
+	//~ cout << test << endl;
+	mur_construction^=toitM;
 	cout << mur_construction << endl;
-	//~ cout << "Operator -= : " << endl;
-	//~ mur_construction-=toit;
-	//~ cout << mur_construction << endl;
-	//~ cout << "Operator += : " << endl;
-	//~ mur_construction+=toit;
-	//~ cout << mur_construction << endl;
   //~ unsigned int largeur(4);
   //~ unsigned int profondeur(3);
   //~ unsigned int hauteur(3); // sans le toit
