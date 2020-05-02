95b21aab7ae40fee7ce88664958caea455377d14
mappleKeuj <34816176+mappleKeuj@users.noreply.github.com>
2020-05-02 18:02:34 -0400

    cuisine.cpp

--- cuisine/cuisine/cuisine.cpp
+++ cuisine/cuisine/cuisine.cpp
@@ -7,8 +7,9 @@ using namespace std;
 /*****************************************************
   * Compléter le code à partir d'ici
  *****************************************************/
-class Produit {
-  private:
+class Produit 
+{
+  protected:
       string m_nom;
       string m_unite;
 
@@ -20,15 +21,14 @@ class Produit {
 
       string toString() const;
 
-      void test();
+      virtual const Produit* adapter(double n) const {
+          return this;
+      }
 };
 string Produit::getNom() const { return m_nom; }
 string Produit::getUnite() const { return m_unite; }
-string Produit::toString() const { return m_nom; }
-void Produit::test()
-{
-    cout << "asdad" << endl;
-}
+string Produit::toString() const { return getNom(); }
+
 /* ----- Ingredient -----*/
 class Ingredient
 {
@@ -51,7 +51,10 @@ const Produit& Ingredient::getProduit() const { return m_produit; }
 
 void Ingredient::descriptionAdaptee()
 {
-    cout << getQuantite() << " " << getProduit().getUnite() << " de " << getProduit().toString() << endl;
+    const Produit* ptr = m_produit.adapter(m_quantite);
+
+    //string str = to_string(getQuantite()) << " " << getProduit().getUnite() << " de " << ptr->toString();
+    //cout << getQuantite() << " " << getProduit().getUnite() << " de " << getProduit().toString() << endl;
 }
 /* ----------------------------------*/
 
@@ -59,19 +62,15 @@ void Ingredient::descriptionAdaptee()
 class Recette
 {
 public:
-    Recette(string nom, double nbFois_ = 1.0f) :m_nom(nom), nbFois_(nbFois_) 
-    {
-        for (auto ingredient : liste)
-        {
-
-        }
-    };
+    Recette(string nom, double nbFois_ = 1.0f) :m_nom(nom), nbFois_(nbFois_) {};
 
     void ajouter(const Produit& p, double quantite);
 
     Recette adapter(double n);
 
     string toString();
+
+    double quantiteTotale(string nom) const;
 
 private:
     string m_nom;
@@ -87,36 +86,81 @@ void Recette::ajouter(const Produit& p, double quantite)
 
 Recette Recette::adapter(double n)
 {
-    Recette r(m_nom, nbFois_ * n);
+    Recette recette_adapte(m_nom, nbFois_ * n);
 
-    return r;
+    for (auto ingredient : liste)
+    {
+        recette_adapte.ajouter(ingredient.getProduit(), ingredient.getQuantite());
+    }
+
+    return recette_adapte;
 }
 
 string Recette::toString() 
 {
-    string str = "Recette " + m_nom + " x " + to_string(nbFois_) + " :\n";
+    string str = "  Recette " + m_nom + " x " + to_string(nbFois_) + " :";
 
     int nbIngredient = 0;
     for (auto ingredient : liste)
     {
         nbIngredient++;
-        str = str + to_string(nbIngredient) + ". " + ingredient.getProduit().toString() + "\n";
- 
+        str = str + "\n  " + to_string(nbIngredient) + ". " + to_string(ingredient.getQuantite()) + " " + ingredient.getProduit().getUnite() + " de " + ingredient.getProduit().toString();
     }
 
     return str;
 }
+
+double Recette::quantiteTotale(string nom) const {
+    for (auto const i : liste) {
+        if (i.getProduit().getNom() == nom) {
+            return i.getQuantite();
+        }
+    }
+    return 0.0;
+}
 /* -----------------------------------*/
 
 /* ----- ProduitCuisine -----*/
-//class ProduitCuisine : public Produit
-//{
-//public:
-//    ProduitCuisine : public Produit();
-//
-//private:
-//
-//};
+class ProduitCuisine : public Produit
+{
+public:
+    ProduitCuisine(string nom, string unite = "portion(s)") : Produit(nom, unite), m_recette(nom), m_nom(nom) {};
+
+    void ajouterARecette(const Produit& produit, double quantite);
+    void ajouterARecette(const ProduitCuisine& produitCuisine, double quantite);
+
+    const ProduitCuisine* adapter(double n);
+
+    string toString();
+
+private:
+    string m_nom;
+    Recette m_recette;
+};
+
+void ProduitCuisine::ajouterARecette(const Produit& produit, double quantite)
+{
+    this->m_recette.ajouter(produit, quantite);
+}
+
+void ProduitCuisine::ajouterARecette(const ProduitCuisine& produitCuisine, double quantite)
+{  
+    this->m_recette.ajouter(produitCuisine, quantite);
+}
+
+const ProduitCuisine* ProduitCuisine::adapter(double n)
+{
+    ProduitCuisine newProduitCuisine(m_nom);
+    newProduitCuisine.m_recette = m_recette.adapter(n);
+
+    return &newProduitCuisine;
+}
+
+string ProduitCuisine::toString()
+{
+    string str = Produit::toString() + "\n" + m_recette.toString();
+    return str;
+}
 
 
  /*******************************************
@@ -142,32 +186,32 @@ int main()
     Produit amandesMoulues("amandes moulues", "grammes");
     Produit extraitAmandes("extrait d'amandes", "gouttes");
 
-    //ProduitCuisine glacage("glaçage au chocolat");
-    //// recette pour une portion de glaçage:
-    //glacage.ajouterARecette(chocolatNoir, 200);
-    //glacage.ajouterARecette(beurre, 25);
-    //glacage.ajouterARecette(sucreGlace, 100);
-    //cout << glacage.toString() << endl;
+    ProduitCuisine glacage("glaçage au chocolat");
+    // recette pour une portion de glaçage:
+    glacage.ajouterARecette(chocolatNoir, 200);
+    glacage.ajouterARecette(beurre, 25);
+    glacage.ajouterARecette(sucreGlace, 100);
+    cout << glacage.toString() << endl;
 
-    //ProduitCuisine glacageParfume("glaçage au chocolat parfumé");
-    //// besoin de 1 portions de glaçage au chocolat et de 2 gouttes
-    //// d'extrait d'amandes pour 1 portion de glaçage parfumé
+    ProduitCuisine glacageParfume("glaçage au chocolat parfumé");
+    // besoin de 1 portions de glaçage au chocolat et de 2 gouttes
+    // d'extrait d'amandes pour 1 portion de glaçage parfumé
 
-    //glacageParfume.ajouterARecette(extraitAmandes, 2);
-    //glacageParfume.ajouterARecette(glacage, 1);
-    //cout << glacageParfume.toString() << endl;
+    glacageParfume.ajouterARecette(extraitAmandes, 2);
+    glacageParfume.ajouterARecette(glacage, 1);
+    cout << glacageParfume.toString() << endl;
 
     Recette recette("tourte glacée au chocolat");
     recette.ajouter(oeufs, 5);
     recette.ajouter(farine, 150);
     recette.ajouter(beurre, 100);
     recette.ajouter(amandesMoulues, 50);
-    //recette.ajouter(glacageParfume, 2);
+    recette.ajouter(glacageParfume, 2);
 
-    //cout << "===  Recette finale  =====" << endl;
-    //cout << recette.toString() << endl;
+    cout << "===  Recette finale  =====" << endl;
+    cout << recette.toString() << endl;
     //afficherQuantiteTotale(recette, beurre);
-    //cout << endl;
+    cout << endl;
 
     //// double recette
     Recette doubleRecette = recette.adapter(2);
